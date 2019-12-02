62d58a54fe508bb303ef353929147b1003cf6efe
David Paskevic <davids.paskevics@gmail.com>
2019-12-03 00:46:36 +0100

    Update ghidra to 9.1

--- pkgs/tools/security/ghidra/default.nix
+++ pkgs/tools/security/ghidra/default.nix
@@ -6,11 +6,11 @@
 
 in stdenv.mkDerivation {
 
-  name = "ghidra-9.0.4";
+  name = "ghidra-9.1";
 
   src = fetchurl {
-    url = https://ghidra-sre.org/ghidra_9.0.4_PUBLIC_20190516.zip;
-    sha256 = "1gqqxk57hswwgr97qisqivcfgjdxjipfdshyh4r76dyrfpa0q3d5";
+    url = https://www.ghidra-sre.org/ghidra_9.1_PUBLIC_20191023.zip;
+    sha256 = "29d130dfe85da6ec45dfbf68a344506a8fdcc7cfe7f64a3e7ffb210052d1875e";
   };
 
   nativeBuildInputs = [
